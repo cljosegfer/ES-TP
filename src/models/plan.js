053958dc15dc4be7b432b329cf3667f1be49f619
josegfer <macaverde604@gmail.com>
2021-01-19 03:23:05 -0300

    codigo base

---
+++ src/models/plan.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const mongoose_paginate = require('mongoose-paginate')
+
+const plan_schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+plan_schema.plugin(mongoose_paginate)
+
+mongoose.model('plan', plan_schema)
