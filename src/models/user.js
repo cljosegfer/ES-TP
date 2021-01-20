a1254bb74eb25ec4d7856478efd90a1d5f468a93
Luiz Eduardo Penido <37006130+LuizPenido@users.noreply.github.com>
2021-01-20 16:09:32 -0300

    Create user.js
    'Schema' do usuário

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const user_schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+            type: String,
+            required: true,
+    },
+    date_of_birth: {
+        type: Date,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female"],
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+mongoose.Model('user', user_schema);
