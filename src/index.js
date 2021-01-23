6c88d1c98fdda0fec7dab2aabe26ffb411cf1274
Ábner Neves <abnermneves@gmail.com>
2021-01-23 19:07:28 -0300

    webpack e babel configurados

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const require_dir = require('require-dir')
+
+// start
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// mongo db
+mongoose.connect("mongodb://localhost:27017/estp", 
+    { useNewUrlParser: true }, 
+    { useUnifiedTopology: true },
+) //mongo warning
+require_dir('./src/models')
+
+// routes
+app.use('/', require('./routes'))
+
+
+app.listen(3001) // porta 3001
