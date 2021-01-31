13862d48b7810eea2a01753376a7d04a2364dc38
Ana Izabel <anaizarezende@gmail.com>
2021-01-31 00:02:57 -0300

    wip

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const require_dir = require('require-dir')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const require_dir = require("require-dir");
 
 // start
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 // mongo db
-mongoose.connect("mongodb://localhost:27017/estp", 
-    { useNewUrlParser: true }, 
-    { useUnifiedTopology: true },
-) //mongo warning
-require_dir('./src/models')
+mongoose
+  .connect(
+    "mongodb://localhost:27017/estp",
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  )
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => {
+    console.log("That didn't work");
+  }); //mongo warning
+require_dir("./src/models");
 
 // routes
-app.use('/', require('./src/routes'))
+app.use("/", require("./src/routes"));
 
-
-app.listen(3001) // porta 3001
+app.listen(3001); // porta 3001
